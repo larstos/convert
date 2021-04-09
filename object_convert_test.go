e1d64d3a58f5eb84a047909a82060917f6a0b31c
larstos <shuo@zhaoshuodeMacBook-Pro.local>
2021-04-09 11:22:14 +0800

    [feture] refine func returns

--- object_convert_test.go
+++ object_convert_test.go
@@ -1,1 +1,31 @@
 package convert_pkg
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGetDataStructFilled(t *testing.T) {
+	Convey("test get data struct field fill type will not change", t, func() {
+		Convey("test struct and pointer", func() {
+			type unNameStruct struct {
+				Name string `json:"name"`
+			}
+			//struct
+			ret, err := GetDataStructFilled(reflect.TypeOf(unNameStruct{}), map[string]interface{}{"name": 1})
+			So(err, ShouldBeNil)
+			So(ret, ShouldResemble, unNameStruct{
+				Name: "1",
+			})
+			//pointer
+			ret, err = GetDataStructFilled(reflect.TypeOf(&unNameStruct{}), map[string]interface{}{"name": 1})
+			So(err, ShouldBeNil)
+			So(ret, ShouldResemble, &unNameStruct{
+				Name: "1",
+			})
+		})
+
+	})
+}
